30b686299527f0853fd55330debbf2f7a0e93be7
Lvdou-Jack <rs249310509@163.com>
2018-10-29 14:47:06 +0800

    happy new year!
    hiahiahia

---
+++ CharpterOne/src/py0101.py
@@ -0,0 +1,13 @@
+#新年贺卡
+#py0101.py
+#2018.10.29
+#
+name1 = input("请输入收卡人：")
+name2 = input("请输入送卡人：")
+print("######################")
+print(name1)
+print()
+print("新年快乐！")
+print()
+print("           Yours",name2)
+print("######################")
